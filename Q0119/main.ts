80fa18606dff19fca84613d1f8a6703b72c4e290
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-13 18:09:00 +0500

    100-days-coding-challenge

--- Q0119/main.ts
+++ Q0119/main.ts
@@ -1,29 +1,10 @@
-// Question 117: Implement a switch statement that evaluates an expression and uses the default case if none of the cases match.
+// Question 119: Create a while loop that logs "Hello, World!" 5 times.
 
-// This function evaluates a grade and logs the corresponding remark
-function evaluateGrade(grade: string): void {
-    switch (grade) {
-      case "A":
-        console.log("Excellent");
-        break;
-      case "B":
-        console.log("Good");
-        break;
-      case "C":
-        console.log("Fair");
-        break; MediaDeviceInfo.toString
-      case "D":
-        console.log("Poor");
-        break;
-      case "F":
-        console.log("Fail");
-        break;
-      default:
-        console.log("Invalid grade");
-        break;
-    }
-  }
-  
-  evaluateGrade("B"); // Outputs: Good
-  evaluateGrade("I"); // Outputs: Invalid
-  // Uses a switch statement to evaluate the grade and defaults to "Invalid grade" if no match is found.
+// Initializes a counter
+let count: number = 0;
+// This while loop runs until count is 5
+while (count < 5) {
+  console.log("Hello, World!"); // Logs "Hello, World!"
+  count++; // Increments count by 1
+}
+// Repeats logging "Hello, World!" 5 times, once per loop iteration.
