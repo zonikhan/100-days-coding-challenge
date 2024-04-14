6d4239223269fbbffad5d9ece4e41e1420671ce4
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-14 11:41:30 +0500

    100-days-coding-challeng

---
+++ Q0130/math.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = void 0;
+var add = function (a, b) { return a + b; };
+exports.add = add;
+// This line exports an add function in main.ts
